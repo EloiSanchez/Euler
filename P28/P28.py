451f76a94f8eb327911e34a76e7cc2dc3ccc900a
Eloi Sanchez <eloi@pop-os.localdomain>
2021-03-10 15:17:39 +0100

    Added problem 28

---
+++ P28/P28.py
@@ -0,0 +1,33 @@
+
+"""
+Starting with the number 1 and moving to the right in a clockwise direction a 5 
+by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed 
+in the same way?
+"""
+
+
+def square(n):
+    if n % 2 == 0:
+        print("The square must be of uneven length.")
+        quit()
+    
+    number = 1
+    total = 1
+    for i in range(3, n + 1, 2):
+        for j in range(4):
+            number += i - 1
+            total += number
+    return total
+
+k = 1001
+print("For n = {} the sum of the elements of the diagonals is {}.".format(k, square(k)))
